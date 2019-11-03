4c4eaf9528a0c8ebd4581633db5825c555efa53e
王自强 <wangziqiang.carl@bytedance.com>
2019-11-03 18:41:54 +0800

    fix(sensor): 修复类型定义问题

--- index.d.ts
+++ index.d.ts
@@ -1,10 +1,10 @@
 /*
  * @description:
- * @author: wangziqiang.carl@bytedance.com
+ * @author: 814120507@qq.com
  * @since: 2019-10-28 22:54:22
  */
 
-export type remoteSensor = (
+export default function remoteSensor(
   url: string,
   callback: (valid: boolean) => void
-) => void;
+): void;
